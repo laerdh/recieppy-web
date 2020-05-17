de38e80a0add1718896d51169dd10dded32ae41f
Lars Dahl <laerdh@gmail.com>
2020-05-17 20:26:48 +0200

    Add functionality for filtering recipes

--- src/components/add-recipe/AddRecipe.tsx
+++ src/components/add-recipe/AddRecipe.tsx
@@ -1,9 +1,10 @@
-import React, { useEffect } from 'react'
+import React, { useEffect, useState } from 'react'
 import styles from './AddRecipe.module.css'
 import Spinner from '../spinner/Spinner'
 import { RecipeItemsState } from '../../context/RecipeContext'
 import CardView, { CardViewType } from '../card/CardView'
 import SearchBar from '../searchbar/SearchBar'
+import { Recipe } from '../../models/Recipe'
 
 export interface AddRecipeProps {
     state: RecipeItemsState
@@ -13,24 +14,47 @@ export interface AddRecipeProps {
 }
 
 const AddRecipe = ({ state, fetchRecipes, addRecipe }: AddRecipeProps) => {
-
+    const [isFiltering, setIsFiltering] = useState(false)
+    const [filteredRecipeItems, setFilteredRecipeItems] = useState<Recipe[]>([])
+    
     useEffect(() => {
         if (state.recipeItems.length === 0 && !state.isLoading) {
             fetchRecipes()
         }
+
+        if (!isFiltering && state.recipeItems.length > 0) {
+            setFilteredRecipeItems(state.recipeItems)
+        }
     })
+
+    function filterRecipeItems(query: string) {
+        if (query.length === 0) {
+            setFilteredRecipeItems(state.recipeItems)
+            setIsFiltering(false)
+        } else {
+            setIsFiltering(true)
+            
+            const filteredList = state.recipeItems.filter(item => {
+                const titleString = item.title.toLowerCase()
+                const queryString = query.toLowerCase().trim()
+                return titleString.includes(queryString)
+            })
+
+            setFilteredRecipeItems(filteredList)
+        }
+    }
 
     return (
         <div className={styles.pageContainer}>
             <h2 id={styles.title}>Legg til oppskrift</h2>
             <div className="divider" style={{ margin: '8px 0 16px 0'}}></div>
-            <SearchBar />
+            <SearchBar onInputChanged={filterRecipeItems} />
             <div className={styles.recipeContainer}>
                 {
                     state.isLoading ?
                         <Spinner />
                         :
-                        state.recipeItems.map(recipe => {
+                        filteredRecipeItems.map(recipe => {
                             return (
                                 <CardView key={recipe.id} type={CardViewType.Small} imageUrl={recipe.imageUrl} onClick={() => { addRecipe(recipe.id) }}>
                                     <h3 className={styles.cardTitle}>{recipe.title}</h3>
--- src/components/searchbar/SearchBar.tsx
+++ src/components/searchbar/SearchBar.tsx
@@ -1,12 +1,20 @@
-import React from 'react'
+import React, { ChangeEvent } from 'react'
 import styles from './SearchBar.module.css'
 import SearchIcon from '../../assets/images/search.svg'
 
-const SearchBar = (props: any) => {
+export interface SearchBarProps {
+    onInputChanged: (text: string) => void
+}
+
+const SearchBar = ({ onInputChanged }: SearchBarProps) => {
     return (
         <div className={`${styles.innerAddon} ${styles.leftAddon}`}>
             <img src={SearchIcon} className={styles.searchIcon} alt="Søk" />
-            <input className={styles.input} type="text" placeholder="Søk" />
+            <input
+                className={styles.input}
+                type="text"
+                placeholder="Søk"
+                onChange={(event: React.ChangeEvent<HTMLInputElement>) => onInputChanged(event.target.value)} />
         </div>
     )
 }
